c8e8ccd3e9bb3968e26b3b8355e89c4cdd00acb7
Mark Fitzpatrick <markfitz815@gmail.com>
2018-11-28 18:38:20 +0000

    retrieve input from chatInputText

--- public/scripts/indexMain.js
+++ public/scripts/indexMain.js
@@ -7,8 +7,20 @@ function initApp(){
 const vm = new Vue({
     el: '#app',
     data: {
-        test: 'hi-ho'
+        userMessage: ''
+    },
+    methods: {
+        checkMessage: function() {
+            if(this.userMessage.length > 0) {
+                console.log(this.userMessage)
+                const thisMessage = this.userMessage;
+                vm.userMessage = '';
+            } 
+        }
+    },
+    computed: {
     }
+
 })
 
 window.onload = initApp;
